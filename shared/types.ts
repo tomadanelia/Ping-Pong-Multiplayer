af26196cdfc695b2f25aee744406d5ab7d324a71
Toma Danelia <danelia.toma@gmail.com>
2025-06-02 16:34:14 +0400

    added shared types

---
+++ shared/types.ts
@@ -0,0 +1,33 @@
+export type PlayerId = string;
+export interface PlayerInfo {
+  id: PlayerId;
+  name: string;
+}
+export interface Paddle {
+    id: PlayerId;
+    x: number;
+    y: number; 
+    width: number;
+    height: number;
+}
+export interface Ball {
+    position: { x: number; y: number };
+    velocity: { x: number; y: number };
+    radius: number;
+}
+export interface GameState {
+    players: Paddle[];
+    ball: Ball;
+    score: { [playerId: PlayerId]: number }; 
+    gameOver: boolean;
+    winner?: PlayerId;
+}
+export interface GameSession {
+ id: string;
+ bottomPlayerId: PlayerId;
+ topPlayerId: PlayerId;
+    state: GameState;
+    started: boolean;
+    createdAt: Date;
+gameState: GameState;
+}
