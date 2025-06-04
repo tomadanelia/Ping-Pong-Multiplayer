282f8e36f300f4a3a7fe8aef8fe019be29934d78
Toma Danelia <danelia.toma@gmail.com>
2025-06-04 23:08:01 +0400

    added and paddle component

---
+++ client/src/components/Paddle.tsx
@@ -0,0 +1,22 @@
+interface PaddleProps {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+}
+
+function Paddle({ x, y, width, height }: PaddleProps) {
+    const paddleStyle: React.CSSProperties = {
+        position: 'absolute',
+        left: `${x}px`,
+        top: `${y}px`,
+        width: `${width}px`,
+        height: `${height}px`
+    };
+
+    return (
+        <div style={paddleStyle}></div>
+    );
+}
+
+export default Paddle;
